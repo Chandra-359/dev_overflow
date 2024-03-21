70de454483224da3adcd02a69a959d2020104181
chandravamsireddy <chandravamsi169@gmail.com>
2024-03-21 17:53:24 -0400

    Update layout: add metadata, fonts, and ClerkProvider configuration
    * Add metadata for title, description, and icons
    * Import Inter and Space_Grotesk fonts
    * Configure ClerkProvider appearance
    * Update body tag to include font classes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,48 @@
-import { ClerkProvider } from '@clerk/nextjs'
-import './globals.css'
-import React from  'react';
- 
+import React from "react";
+import { ClerkProvider } from "@clerk/nextjs";
+import "./globals.css";
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Dev-OverFlow",
+  description:
+    "Dev-OverFlow is a community-driven Q&A platform for developers, providing insights, solutions, and collaboration opportunities across a wide range of programming languages and technologies. Whether you're stuck on a coding issue, seeking best practices, or sharing your expertise, Dev-OverFlow is your go-to hub for all things development. Join our growing community to learn, share, and advance your development skills.",
+  icons: {
+    icon: "/public/assets/images/site-logo.svg",
+  },
+};
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: "primary-text-gradient hover:text-primary-500",
+        },
+      }}
+    >
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
