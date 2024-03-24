d4bbc0978db189632fd9e437cfa99d697b02eba5
chandravamsireddy <chandravamsi169@gmail.com>
2024-03-24 13:31:17 -0400

    Add MobielNav, LeftSideBar

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,35 @@
+import Navbar from "@/components/shared/navbar/Navbar";
+import React from "react";
+import LeftSideBar from "@/components/shared/navbar/LeftSideBar";
+
+// sm: applies to screens ≥640px wide.
+// md: applies to screens ≥768px wide.
+// lg: applies to screens ≥1024px wide.
+// xl: applies to screens ≥1280px wide.
+// 2xl: applies to screens ≥1536px wide.
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="background-light850_dark100 relative">
+      <Navbar />
+      <div className="flex">
+        {/* max-sm:hidden class helps in hiding the leftsidebar  */}
+        <section className="custom-scrollbar background-light900_dark200 light-border sticky left-0 top-0 flex h-screen w-fit flex-col justify-between  overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]">
+          <LeftSideBar />
+        </section>
+
+        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
+          <div className="mx-auto w-full max-w-5xl">{children}</div>
+        </section>
+
+        <section className="custom-scrollbar background-light900_dark200 light-border sticky right-0 top-0 flex h-screen w-[350px] flex-col gap-6 overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden">
+          RightSidebar
+          </section>
+
+      </div>
+      Toaster
+    </main>
+  );
+};
+
+export default Layout;
---
+++ components/shared/navbar/LeftSideBar.tsx
@@ -0,0 +1,92 @@
+"use client";
+import React from "react";
+import { SignedOut } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import { sidebarLinks } from "@/constants";
+import { usePathname } from "next/navigation";
+import Image from "next/image";
+import Link from "next/link";
+
+const LeftSideBar = () => {
+  const NavContent = () => {
+    const pathname = usePathname();
+    return (
+      <>
+        {sidebarLinks.map((item) => {
+          const isActive =
+            (pathname.includes(item.route) && item.route.length > 1) ||
+            pathname === item.route;
+
+          return (
+            <React.Fragment key={item.route}>
+              <Link
+                href={item.route}
+                className={`${
+                  isActive
+                    ? "primary-gradient rounded-lg text-light-900"
+                    : "text-dark300_light900"
+                } flex items-center justify-start gap-4 bg-transparent p-4`}
+              >
+                <Image
+                  src={item.imgURL}
+                  alt={item.label}
+                  width={20}
+                  height={20}
+                  className={`${isActive ? "" : "invert-colors"}`}
+                />
+                <p
+                  className={`${isActive ? "base-bold" : "base-medium"} max-lg:hidden`}
+                >
+                  {item.label}
+                </p>
+              </Link>
+            </React.Fragment>
+          );
+        })}
+      </>
+    );
+  };
+
+  return (
+    <>
+      <div className="flex flex-1 flex-col gap-6">
+        <NavContent />
+      </div>
+
+      <div className="flex flex-col gap-3">
+        <SignedOut>
+          {/* <div className="flex flex-col gap-3"> */}
+          <Link href="/sign-in">
+            <Button className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+              <Image
+                src="/assets/icons/account.svg"
+                alt="login"
+                width={20}
+                height={20}
+                className="invert-colors lg:hidden"
+              />
+              <span className="primary-text-gradient max-lg:hidden">
+                Log In
+              </span>
+            </Button>
+          </Link>
+          <Link href="/sign-up">
+            <Button className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+              <Image
+                src="/assets/icons/sign-up.svg"
+                alt="signin"
+                width={20}
+                height={20}
+                className="invert-colors lg:hidden"
+              />
+              <span className="max-lg:hidden">Sign Up</span>
+            </Button>
+          </Link>
+          {/* </div> */}
+        </SignedOut>
+      </div>
+    </>
+  );
+};
+
+export default LeftSideBar;
